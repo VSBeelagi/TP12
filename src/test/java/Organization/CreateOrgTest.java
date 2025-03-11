6297590a8cec57edeecf6825509c07a590ad5991
VSBeelagi <vijayalaxmibeelagi10@gmail.com>
2025-03-11 12:54:49 +0530

    org created

--- src/test/java/Organization/CreateOrgTest.java
+++ src/test/java/Organization/CreateOrgTest.java
@@ -1,5 +1,12 @@
 package Organization;
 
+import org.testng.annotations.Test;
+
 public class CreateOrgTest {
 
+	@Test
+	public void createOrg()
+	{
+		System.out.println("--- createOrg ----");
+	}
 }
